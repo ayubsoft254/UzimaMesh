3124d3affbd47b51f3aa0d9c7d4526bf44f7129b
ayubsoft254 <henryoayub15@gmail.com>
2026-02-21 21:12:40 +0300

    feat: include user seeding in startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,10 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# 3. Start Gunicorn
+# 3. Seed test users
+echo "Seeding test users..."
+python seed_users.py
+
+# 4. Start Gunicorn
 echo "Starting Gunicorn..."
 gunicorn --bind=0.0.0.0 --timeout 600 uzima_mesh.wsgi
